1423cdb41b9a1d562edba855f1c651b6d2f0a96c
zreonx <zrodayrit@gmail.com>
2024-11-03 23:14:30 +0800

    added cart button

--- src/components/CartButton.tsx
+++ src/components/CartButton.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import { Button } from './ui/button';
+import { ShoppingCart } from 'lucide-react';
+import { Link } from 'react-router-dom';
 
 function CartButton() {
+  const numItemsCart = 5;
   return (
-    <div>
-      CartButton
-    </div>
-  )
+    <Button
+      asChild
+      variant="outline"
+      size="icon"
+      className="flex justify-center items-center relative"
+    >
+      <Link to="/cart">
+        <ShoppingCart />{' '}
+        <span className="absolute -top-3 -right-3 bg-primary text-white rounded-full h-6 w-6 flex items-center justify-center text-xs">
+          {numItemsCart}
+        </span>
+      </Link>
+    </Button>
+  );
 }
 
-export default CartButton
+export default CartButton;
