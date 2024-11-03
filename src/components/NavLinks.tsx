61ba1459df3af693cacc165566738b33054842dc
zreonx <zrodayrit@gmail.com>
2024-11-03 09:15:03 +0800

    added navlinks for large device

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import { links } from '@/utils';
+import { NavLink } from 'react-router-dom';
 
 function NavLinks() {
-  return <div>NavLinks</div>;
+  return <div className='hidden lg:flex justify-start items-center gap-x-4'>
+    {links.map((link) => {
+      return <NavLink to={link.href} key={link.label} className={({isActive}) => {
+        return `capitalize font-normal tracking-wide ${isActive}? 'text-primary' : ''`
+      }}>{link.label}</NavLink>
+    })}
+  </div>;
 }
 
 export default NavLinks;
