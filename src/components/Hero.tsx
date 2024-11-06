a3e49714dcdbc2ff3d8a8f93c9336365e10fd9e5
zreonx <zrodayrit@gmail.com>
2024-11-06 12:54:53 +0800

    added carousel and hero images to the carousel and added hero secton to the landing page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,26 +1,5 @@
-import { Link } from 'react-router-dom';
-import { Button } from './ui/button';
-import HeroCarousel from './HeroCarousel';
-
 function Hero() {
-  return (
-    <section className="grid grid-cols-1 lg:grid-cols-2 gap-24 items-center">
-      <div>
-        <h1 className="max-w-2xl font-bold text-4xl tracking-tight sm:text-6xl">
-          We are changing the way people shop
-        </h1>
-        <p className="mt-8 max-w-xl text-xl leading-8">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur quis
-          quo perferendis illo tempora dolorem facere distinctio magnam porro
-          iste.
-        </p>
-        <Button asChild size='lg' className='mt-10'>
-          <Link to='/products'>Our Products</Link>
-        </Button>
-      </div>
-      <HeroCarousel />
-    </section>
-  );
+  return <div>Hero</div>;
 }
 
-export default Hero;
+export default Hero
