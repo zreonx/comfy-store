aabacb3361a85374c33443b10c463157ba06c6a1
zreonx <zrodayrit@gmail.com>
2024-11-14 07:26:31 +0800

    added shadcn loading skeleton and partner with useNavigation of react router to get the loading state and show the shadcn skeleton on loading

--- src/pages/HomeLayout.tsx
+++ src/pages/HomeLayout.tsx
@@ -1,13 +1,19 @@
-import { Header, Navbar } from '@/components';
-import { Outlet } from 'react-router-dom';
+import { Header, Loading, Navbar } from '@/components';
+import { Outlet, useNavigation } from 'react-router-dom';
+
 
 function HomeLayout() {
+  const navigation = useNavigation();
+
+  const isPageLoading = navigation.state === 'loading';
+
   return (
     <>
       <Header />
       <Navbar />
+
       <div className="align-element py-20">
-        <Outlet />
+        {isPageLoading ? <Loading /> : <Outlet />}
       </div>
     </>
   );
